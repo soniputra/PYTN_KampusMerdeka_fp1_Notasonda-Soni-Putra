042c57bae19e3985293d2e179ab693b48e9abb46
Notasonda Soni Putra <70831424+soniputra@users.noreply.github.com>
2022-05-10 15:43:14 +0700

    Final Project 1

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, url_for
 import numpy as np
 import pickle
+import pandas as pd
 
 # Decision Tree Pickle
 model1 = pickle.load(open("model/model_dt.pkl", "rb"))
 # Linear Regression Pickle
 model2 = pickle.load(open("model/model_lr.pkl", "rb"))
 # Random Forest Pickle
-model3 = pickle.load(open("model/model_rf.pkl", "rb"))
+#model3 = pickle.load(open("model/model_rf.pkl", "rb"))
 
 
 app = Flask(__name__, template_folder="templates")
@@ -30,7 +31,7 @@ def predict_1():
 
     output_1 = round(prediction_1[0], 2)
 
-    return render_template('index.html', prediction_text_1='Prediksi Tarif Decision Tree yaitu : $ {} dengan tingkat akurasi 95%'.format(output_1))
+    return render_template('index.html', prediction_text_1='Prediksi Tarif Decision Tree yaitu : $ {}'.format(output_1))
 
 # Linear regression
 
@@ -46,23 +47,7 @@ def predict_2():
 
     output_2 = round(prediction_2[0], 2)
 
-    return render_template('index.html', prediction_text_2='Prediksi Tarif Linear Regression yaitu : $ {} dengan tingkat akurasi 52%'.format(output_2))
-
-# Random Forest
-
-
-@app.route('/predict_3', methods=['POST'])
-def predict_3():
-    '''
-    For rendering results on HTML GUI
-    '''
-    features_3 = [z for z in request.form.values()]
-    final_features_3 = [np.array(features_3)]
-    prediction_3 = model3.predict(final_features_3)
-
-    output_3 = round(prediction_3[0], 2)
-
-    return render_template('index.html', prediction_text_3='Prediksi Tarif Random Forest yaitu : $ {} dengan tingkat akurasi 96%'.format(output_3))
+    return render_template('index.html', prediction_text_2='Prediksi Tarif Linear Regression yaitu : $ {}'.format(output_2))
 
 
 if __name__ == '__main__':
